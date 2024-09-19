b562b42af08670be8492307dd161960e0bfd754e
Ismail Faizan <ismailfaizan555@gmail.com>
2024-09-19 17:13:47 +0400

    setup-commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-3xl font-bold underline">
+            Hello world!!
         </p>
         <a
           className="App-link"
